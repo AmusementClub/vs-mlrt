d0bb26989ab2ebe19647b06102f3cfbf1ce88ce5
WolframRhodium <WolframRhodium@users.noreply.github.com>
2023-11-11 20:36:42 +0800

    scripts/vsmlrt.py: added support for RIFE v4.11 model

--- scripts/vsmlrt.py
+++ scripts/vsmlrt.py
@@ -1,4 +1,4 @@
-__version__ = "3.18.6"
+__version__ = "3.18.7"
 
 __all__ = [
     "Backend", "BackendV2",
@@ -816,6 +816,7 @@ class RIFEModel(enum.IntEnum):
     v4_8 = 48
     v4_9 = 49
     v4_10 = 410
+    v4_11 = 411
 
 
 def RIFEMerge(
@@ -924,7 +925,7 @@ def RIFEMerge(
     if implementation_version == 2:
         if isinstance(backend, Backend.TRT):
             # https://github.com/AmusementClub/vs-mlrt/issues/66#issuecomment-1791986979
-            if (4, 0) <= (model_major, model_minor) <= (4, 10):
+            if (4, 0) <= (model_major, model_minor):
                 backend.custom_args.extend([
                     "--precisionConstraints=obey", 
                     "--layerPrecisions=" + (
