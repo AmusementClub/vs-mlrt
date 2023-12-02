2b7c1cbbf0dff3c2fc8f8ebad17bd7f6a9167c62
WolframRhodium <WolframRhodium@users.noreply.github.com>
2023-12-02 08:29:41 +0800

    vstrt/vs_tensorrt.cpp: fix crash when reloading script on TRT 9.1 on Windows
    https://github.com/AmusementClub/vs-mlrt/discussions/65

--- vstrt/vs_tensorrt.cpp
+++ vstrt/vs_tensorrt.cpp
@@ -367,9 +367,12 @@ static void VS_CC vsTrtCreate(
     d->logger.set_verbosity(static_cast<nvinfer1::ILogger::Severity>(verbosity));
 
 #ifdef USE_NVINFER_PLUGIN
+    // related to https://github.com/AmusementClub/vs-mlrt/discussions/65, for unknown reason
+#if !(((NV_TENSORRT_MAJOR * 1000) + (NV_TENSORRT_MINOR * 100) + NV_TENSORRT_PATCH) == 9100 && defined(_WIN32))
     if (!initLibNvInferPlugins(&d->logger, "")) {
         vsapi->logMessage(mtWarning, "vsTrt: Initialize TensorRT plugins failed");
     }
+#endif
 #endif
 
     std::ifstream engine_stream {
