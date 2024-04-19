6b33f0e7e774a7ffdb4733f71cb9c94948bcfa74
WolframRhodium <WolframRhodium@users.noreply.github.com>
2024-04-19 11:32:14 +0800

    common/convert_float_to_float16: optional io type casting

--- common/convert_float_to_float16.h
+++ common/convert_float_to_float16.h
@@ -8,12 +8,14 @@
 
 void convert_float_to_float16(
     ONNX_NAMESPACE::ModelProto & model,
-    bool force_fp16_initializers
-    // , bool keep_io_types = True
-    // , bool disable_shape_infer = True
-    // , const std::optional<std::unordered_set<std::string>> op_block_list = DEFAULT_OP_BLOCK_LIST
-    // , const std::optional<std::unordered_set<std::string>> op_block_list = {}
-    , const std::unordered_set<std::string> & op_block_list
+    bool force_fp16_initializers,
+    // bool keep_io_types = True,
+    // bool disable_shape_infer = True,
+    // const std::optional<std::unordered_set<std::string>> op_block_list = DEFAULT_OP_BLOCK_LIST,
+    // const std::optional<std::unordered_set<std::string>> op_block_list = {},
+    const std::unordered_set<std::string> & op_block_list,
+    bool cast_input = true,
+    bool cast_output = true
 ) noexcept;
 
 #endif
