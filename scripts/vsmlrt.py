68aa56e362d8392985c9d0902ed0df7a4bfd279d
WolframRhodium <WolframRhodium@users.noreply.github.com>
2024-05-24 14:28:36 +0800

    scripts/vsmlrt.py: add support for RIFE v4.17 model

--- scripts/vsmlrt.py
+++ scripts/vsmlrt.py
@@ -1,4 +1,4 @@
-__version__ = "3.21.4"
+__version__ = "3.21.5"
 
 __all__ = [
     "Backend", "BackendV2",
@@ -897,6 +897,7 @@ class RIFEModel(enum.IntEnum):
     v4_15 = 415
     v4_15_lite = 4151
     v4_16_lite = 4161
+    v4_17 = 417
 
 
 def RIFEMerge(
