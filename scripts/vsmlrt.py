80f3f47ddf87a56552b598d8be032684976a7d5b
WolframRhodium <WolframRhodium@users.noreply.github.com>
2025-08-09 13:47:03 +0800

    scripts/vsmlrt.py: add fp16 I/O support for the TRT_RTX backend

--- scripts/vsmlrt.py
+++ scripts/vsmlrt.py
@@ -1,4 +1,4 @@
-__version__ = "3.22.22"
+__version__ = "3.22.23"
 
 __all__ = [
     "Backend", "BackendV2",
@@ -2359,6 +2359,7 @@ def tensorrt_rtx(
     max_tactics: typing.Optional[int] = None,
     tiling_optimization_level: int = 0,
     l2_limit_for_tiling: int = -1,
+    fp16_io: bool = False,
 ) -> str:
 
     # tensort runtime version
@@ -2368,9 +2369,11 @@ def tensorrt_rtx(
         import onnx
         from onnxconverter_common.float16 import convert_float_to_float16
         model = onnx.load(network_path)
-        model = convert_float_to_float16(model, keep_io_types=True)
-        network_path = f"{network_path}_fp16.onnx"
+        model = convert_float_to_float16(model, keep_io_types=not fp16_io)
+        network_path = f"{network_path}_fp16{'_io' if fp16_io else ''}.onnx"
         onnx.save(model, network_path) # TODO
+    elif fp16_io:
+        raise ValueError('tensorrt_rtx: "fp16" must be True.')
 
     engine_path = get_engine_path(
         network_path=network_path,
@@ -2384,8 +2387,8 @@ def tensorrt_rtx(
         static_shape=static_shape,
         tf32=False,
         use_cudnn=False,
-        input_format=0,
-        output_format=0,
+        input_format=int(fp16_io),
+        output_format=int(fp16_io),
         builder_optimization_level=builder_optimization_level,
         max_aux_streams=max_aux_streams,
         short_path=short_path,
@@ -2926,8 +2929,6 @@ def _inference(
             use_cudnn=backend.use_cudnn,
             use_edge_mask_convolutions=backend.use_edge_mask_convolutions,
             input_name=input_name,
-            # input_format=clips[0].format.bits_per_sample == 16,
-            # output_format=backend.output_format,
             builder_optimization_level=backend.builder_optimization_level,
             max_aux_streams=backend.max_aux_streams,
             short_path=backend.short_path,
@@ -2937,6 +2938,11 @@ def _inference(
             max_tactics=backend.max_tactics,
             tiling_optimization_level=backend.tiling_optimization_level,
             l2_limit_for_tiling=backend.l2_limit_for_tiling,
+
+            # the following option is experimental
+            # input_format=clips[0].format.bits_per_sample == 16,
+            # output_format=backend.output_format,
+            fp16_io=clips[0].format.bits_per_sample == 16
         )
         ret = core.trt_rtx.Model(
             clips, engine_path,
